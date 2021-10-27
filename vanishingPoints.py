132768242ba46f7abfe1f56f657f42b4783b803c
NglQ <quarta.angelo@hotmail.it>
2021-10-28 00:25:32 +0200

    The idea of taking account of centroids of the clusters as descriptive of the entire cluster was not as successful as I expected at the beginning. So, I used the clusters as a mean to save computational resources. At the moment I should refactor the code in order to be more readable and useful.

--- vanishingPoints.py
+++ vanishingPoints.py
@@ -8,14 +8,13 @@ import cv2
 import numpy as np
 import math
 
-img = cv2.imread('biblioteca.jpg')
+img = cv2.imread('twoVanPnts.jpg')
 imgOrig = img.copy()
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 cv2.imwrite('gray.jpg',gray)
 
 gray = cv2.GaussianBlur(gray,(3,3),0,0)
-# gray = cv2.medianBlur(gray,5)
 
 filter45=np.array([[-1, -1, 2], [-1, 2, -1], [2, -1, -1]]);
 
@@ -39,7 +38,7 @@ if lines is not None and len(lines) > 0:
             distCat =cv2.norm(np.array([x1,y1]),np.array([x1,y2]),cv2.NORM_L2)
             distHyp = cv2.norm(np.array([x1,y1]),np.array([x2,y2]),cv2.NORM_L2)
             angle = np.arcsin(distCat/distHyp)
-            data.append(np.array([angle,np.cos(2*angle), np.sin(2*angle)]))
+            data.append(np.array([np.cos(angle), np.sin(angle)]))
 
     #cv2.imwrite('houghlines3.jpg',img)
 
@@ -48,29 +47,44 @@ if lines is not None and len(lines) > 0:
 
     data = np.array(data, dtype=np.float32)
 
-    #print(len(data)
-    #print(data)
     nClusters = 10
 
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 1.0)
     ret, label, center = cv2.kmeans(data, nClusters, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
-
+    
+    
 #TODO: find representative lines for each cluster
 
     repLines = []
-
+    intersections = []
+    
     for i in range(nClusters):
         clusterIdx = (label == i)
         clusterData = [data[x] for x in range(data.shape[0]) if clusterIdx[x]]
         clusterLines = lines[clusterIdx]
-        clDiff = [cv2.norm(np.array(x - center[i]),cv2.NORM_L2) for x in clusterData]
-        minLine = clDiff.index(min(clDiff))
+        #clDiff = [cv2.norm(np.array(x - center[i]),cv2.NORM_L2) for x in clusterData]
+        #minLine = clDiff.index(min(clDiff))
         
-        cv2.line(img,(clusterLines[minLine][0],clusterLines[minLine][1]),(clusterLines[minLine][2],clusterLines[minLine][3]),(0,255,0),2)
+        #cv2.line(img,(clusterLines[minLine][0],clusterLines[minLine][1]),(clusterLines[minLine][2],clusterLines[minLine][3]),(0,255,0),2)
         
         #TODO: find a better way to filter 90° angles
         if center[i][1] < 0.984807753 or center[i][1] > -0.984807753: 
-            repLines.append(clusterLines[minLine])
+            for li1 in range(len(clusterLines)):
+                ln1 = clusterLines[li1]
+                m1 = (ln1[3] - ln1[1]) / (ln1[2] - ln1[0]) 
+                q1 = ln1[1] - m1 * ln1[0]
+                for li2 in range(li1,len(clusterLines)):
+                    ln2 = clusterLines[li2]
+                    m2 = (ln2[3] - ln2[1]) / (ln2[2] - ln2[0])
+                    q2 = ln2[1] - m2 * ln2[0]
+            
+                    xint = (q1-q2) / (m2-m1)
+                    yint = m1 * xint + q1
+            #print(xint,yint)
+                    if not math.isnan(xint) and not math.isnan(yint) and not math.isinf(xint) and not math.isinf(yint):
+                        img = cv2.circle(img, (round(xint),round(yint)), radius = 2, color = (255,0,0), thickness = -1)
+                        intersections.append(np.array([xint, yint]))
+            #repLines.append(clusterLines[minLine])
         
         imgTemp = imgOrig.copy()
         for cll in clusterLines:
@@ -78,42 +92,12 @@ if lines is not None and len(lines) > 0:
             cv2.line(imgTemp,(cll[0],cll[1]),(cll[2],cll[3]),(0,0,255),2)
         cv2.imwrite('houghlinesCls'+  str(i) +'.jpg',imgTemp)
             
-            #print(clDiff)
-            
-            #print(minLine)
-            
-
-    cv2.imwrite('houghlinesRep.jpg',img)
-#TODO: find intersection among the lines generated from the real lines closest to the centroids
-    print(len(repLines))
-    intersections = []
-    for li1 in range(len(repLines)):
-        ln1 = repLines[li1]
-        m1 = (ln1[3] - ln1[1]) / (ln1[2] - ln1[0]) 
-        q1 = ln1[1] - m1 * ln1[0]
-        for li2 in range(li1,len(repLines)):
-            ln2 = repLines[li2]
-            m2 = (ln2[3] - ln2[1]) / (ln2[2] - ln2[0])
-            q2 = ln2[1] - m2 * ln2[0]
-            
-            xint = (q1-q2) / (m2-m1)
-            yint = m1 * xint + q1
-            #print(xint,yint)
-            if not math.isnan(xint) and not math.isnan(yint):
-                img = cv2.circle(img, (round(xint),round(yint)), radius = 2, color = (255,0,0), thickness = -1)
-                intersections.append(np.array([xint, yint]))
-    
-    #print(intersections)
     cv2.imwrite("intersections.jpg", img)
     
-# print(center)
-# print(label)
-# print(ret)
-
 #TODO: filter away non-reliable results (good luck with that!!!)
     
     
-    
+    #TODO: use the distance between points as a score of the goodness of the point as a vanishing point
     
     
     
